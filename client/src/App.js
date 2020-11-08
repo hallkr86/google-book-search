d98dda21ae48be7b4b918c36aa2acbc6fe3c1e41
Kesha Hall <k_renee10@yahoo.com>
2020-11-07 19:17:32 -0500

    added exact path in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Search from "./pages/Search";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Saved from "./pages/Saved";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/" component={Saved} />
+        </Switch>
+      </Router>
     </div>
   );
 }
